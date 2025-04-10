e56d9c53c0a7d32812685a953a287092ecdcbcdf
restoker <milthon.doom@gmail.com>
2025-04-09 20:57:51 -0500

    header animations completed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,13 +1,25 @@
 import React, { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap';
+import SplitType from 'split-type';
 
 const Hero = ({ toggleTheme, isDemoReady }) => {
     const containerRef = useRef(null);
     const sectionRef = useRef(null);
     const contentHeader = useRef(null);
+    const titleRef = useRef(null);
+    const subTitleRef = useRef(null);
+    const playRef = useRef(null);
+    const diagonalLines = useRef(null);
+    const line2 = useRef(null);
+    const startNow = useRef(null);
+    const navMenu = useRef(null);
+    const lastSection = useRef(null);
+
     useGSAP(() => {
         if (!isDemoReady) return;
+
+        const text = new SplitType(titleRef.current);
 
         const tl = gsap.timeline();
 
@@ -20,6 +32,40 @@ const Hero = ({ toggleTheme, isDemoReady }) => {
                 opacity: 0,
                 y: -50,
             }, '<')
+            .from(text.chars, {
+                opacity: 0,
+                yPercent: -100,
+                stagger: 0.03,
+            })
+            .from(subTitleRef.current, {
+                opacity: 0,
+                xPercent: -10,
+                delay: 0.2,
+            }, '<')
+            .from(playRef.current, {
+                xPercent: -10,
+                opacity: 0,
+            }, '-=0.1')
+            .from(diagonalLines.current, {
+                xPercent: -10,
+                opacity: 0,
+            }, '-=0.1')
+            .from(line2.current, {
+                xPercent: -10,
+                opacity: 0,
+            }, '-=0.1')
+            .from(startNow.current, {
+                xPercent: -10,
+                opacity: 0,
+            }, '<')
+            .from(navMenu.current, {
+                xPercent: -10,
+                opacity: 0,
+            }, '-=0.1')
+            .from(lastSection.current, {
+                yPercent: -50,
+                opacity: 0,
+            }, '-=0.1')
         // .from()
 
     }, { scope: containerRef, dependencies: [isDemoReady] })
@@ -66,10 +112,10 @@ const Hero = ({ toggleTheme, isDemoReady }) => {
                     className='bg-zinc-950 text-white flex justify-evenly relative'
                 >
                     <div className='flex flex-col justify-evenly'>
-                        <h1 className='text-9xl font-jersey'>g:hub</h1>
-                        <p>Everything you need for <br /> victory-all in one place!</p>
+                        <h1 className='text-9xl font-jersey' ref={titleRef}>g:hub</h1>
+                        <p ref={subTitleRef}>Everything you need for <br /> victory-all in one place!</p>
                     </div>
-                    <div className='flex items-end py-16 px-5 text-3xl'>
+                    <div ref={playRef} className='flex items-end py-10 px-5 text-3xl overflow-hidden'>
                         <p>
                             Play.
                             <br />
@@ -78,28 +124,37 @@ const Hero = ({ toggleTheme, isDemoReady }) => {
                         <img className='' src="/img/line1.svg" alt="linea-1" />
                         <p>Win.</p>
                     </div>
-                    <div>
-                        <img src="/img/lines.svg" alt="lineas" className='' />
+                    <div className='overflow-hidden'>
+                        <img ref={diagonalLines} src="/img/lines.svg" alt="lineas" className='' />
                     </div>
-                    <div className='flex flex-col justify-between py-16'>
-                        <div className='flex justify-evenly'>
+                    <div className='flex flex-col justify-between py-10'>
+                        <div ref={startNow} className='flex justify-evenly'>
                             <p>Go to your gaming <br /> headquarters</p>
                             <p className='text-3xl bg-gradient-to-r from-lime-600 via-lime-400 to-gray-100 text-transparent bg-clip-text'>Start now</p>
                         </div>
-                        <div>
-                            <img src="/img/line2.svg" alt="linea-2" className='' />
+                        <div className='overflow-hidden'>
+                            <img ref={line2} src="/img/line2.svg" alt="linea-2" className='' />
                         </div>
-                        <div className='flex'>
-                            <p>Home</p>
-                            <p>Stats</p>
-                            <p>Community</p>
+                        <div ref={navMenu} className='flex justify-evenly'>
+                            <p className='relative'>
+                                Home <span className='absolute border border-white rounded-4xl -top-5 px-3 text-left'>01</span>
+                            </p>
+                            <p className='relative'>
+                                Stats <span className='absolute border border-white rounded-4xl -top-5 px-3 text-left'>02</span>
+                            </p>
+                            <p className='relative'>
+                                Community <span className='absolute border border-white rounded-4xl -top-5 px-3 text-left'>03</span>
+                            </p>
                         </div>
                     </div>
                 </div>
                 {/* <input type='checkbox' onChange={toggleTheme} checked={isDarkTheme} /> */}
                 {/* <span className='slider-label'>Switch to {!isDarkTheme ? 'dark mode' : 'light mode'}</span> */}
-                <div className='relative'>
+                <div ref={lastSection} className='relative'>
                     <div className='absolute top-10 aspect-square bg-white/10 backdrop-blur-lg h-96 left-10 p-16 flex flex-col justify-center items-center rounded-4xl overflow-hidden z-30'>
+                        <img className='absolute' src="/img/image1.png" alt="imagen1" />
+                        <img className='absolute' src="/img/image2.png" alt="imagen2" />
+                        <img className='absolute' src="/img/image3.png" alt="imagen3" />
                         <p>Your ultimate gaming hub: track stats, find teammates, join tournaments, and level up your skills.</p>
                         <button className='px-6 py-2 mt-10 border border-zinc-950 rounded-4xl hover:cursor-pointer hover:bg-lime-400/50'>
                             More
